58ce5b13a42a91fcdbc56ec7b837ea867721f060
Ricky Grams <rickygrams@Rickys-MacBook-Pro.local>
2017-03-12 19:59:04 -0700

    onclicks and listeners

--- logic.js
+++ logic.js
@@ -1,5 +1,4 @@
 $(document).ready(function() {
-    console.log( "ready!" );
 
     // initailize firebase ************
     var config = {
@@ -8,28 +7,49 @@ $(document).ready(function() {
     databaseURL: "https://train-scheduler-43ae9.firebaseio.com",
     storageBucket: "train-scheduler-43ae9.appspot.com",
     messagingSenderId: "1046913959404"
-  	};
-
-  	firebase.initializeApp(config);
+	};
+	
+	firebase.initializeApp(config);
+	
+	// variable to reference the database
+  	
+  	database = firebase.database();
 
   	// Variables  ************
-  	
-    var trainName = 
-    var destination = 
-    var firstTrainTime = 
-    var frequency = 
-    var nextArrival = 
-    var minutesAway = 
+    
+    var nextArrival = 1235;
+    var minutesAway = 30;
 
 
     // Functions ***************
 
     $("#addTrain").on("click", function() {
     	event.preventDefault();
+    	var trainName = $("#trainNameInput").val().trim();
+    	var destination = $("#destinationInput").val().trim();
+    	var firstTrainTime = $("#firstTrainInput").val().trim();
+    	var frequency = $("#frequencyInput").val().trim();
 
+    	$("input:text").val("");
 
+    	database.ref().push({
+    		trainName: trainName,
+    		destination: destination,
+    		firstTrainTime: firstTrainTime,
+    		frequency: frequency
+    	});
     });
 
+    database.ref().on("child_added", function(snapshot) {
+    	$(".table").find('tbody')
+	    		.append("<tr><td>" + snapshot.val().trainName + "</td><td>" + snapshot.val().destination + "</td><td>" + snapshot.val().frequency + "</td><td>" + nextArrival + "</td><td>" + minutesAway + "</td></tr>")
+	    	 		// .append("<td>" + snapshot.val().destination + "</td>")
+	    				// .append("<td>" + snapshot.val().frequency + "</td>")
+	    				// 	.append("<td>" + nextArrival + "</td>")
+	    				// 		.append("<td>" + minutesAway + "</td></tr>")
+	    							
+
+    });
 
 
 });
